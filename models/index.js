0a6a6942cfbaca2041145c6c33762b5acaeb0c33
parkerrappleye <parker.rappleye1@gmail.com>
2024-03-27 20:30:31 -0600

    set up asociations for each model

--- models/index.js
+++ models/index.js
@@ -4,13 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Set up associations
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
